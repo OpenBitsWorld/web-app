7e6b99fa46ffc43d6686724c5ffb1b737e67c62e
Luca Cervone <you@example.com>
2020-09-17 17:25:20 +0200

    improved the main menu on desktop

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -13,9 +13,115 @@
         <img src="../../assets/logo-white.png" class="navbar-logo" />
       </h4>
     </b-navbar-brand>
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-toggle
+      class="text-white"
+      data-toggle="modal"
+      data-target="test-modal"
+      target="test-modal"
+      v-b-modal.test-modal>
+      <template v-slot:default="{ expanded }">
+        <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+        <b-icon v-else icon="chevron-bar-down"></b-icon>
+      </template>
+    </b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <!-- Right aligned nav items -->
+      <b-navbar-nav
+        v-if="isLogged"
+        class="ml-auto">
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <strong
+              class="text-dark">{{defaultARWallet.address}}
+              <div class="d-inline-block">
+                <b-badge
+                  variant="secondary-color">
+                  Balance: {{defaultARWallet.balanceAR}} AR
+                </b-badge>
+              </div>
+            </strong>
+          </template>
+          <b-dropdown-item>
+            <router-link to="user-profile">Profile</router-link></b-dropdown-item>
+          <b-dropdown-item
+            @click="signOut()">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+      <b-navbar-nav
+        v-else
+        class="ml-auto">
+        <b-nav-item
+          href="#"
+          v-scroll-to="'#why-section'"
+          class="text-white pt-2 d-inline-block"
+          right>
+          Why OpenBits
+        </b-nav-item>
+        <b-nav-item
+          href="#"
+          v-scroll-to="'#how-section'"
+          class="text-white pt-2 d-inline-block"
+          right>
+          How OpenBits Works
+        </b-nav-item>
+        <b-nav-item
+          href="#"
+          v-scroll-to="'#what-section'"
+          class="text-white pt-2 d-inline-block"
+          right>
+          Get Started
+        </b-nav-item>
+        <b-nav-item
+          @click="$router.push('explore-openbits')"
+          class="text-white pt-2 d-inline-block"
+          right>
+          Explore OpenBits
+        </b-nav-item>
+        <div class="social-nav pt-2 px-2">
+          <b-nav-item
+            target="_blank"
+            href="https://discord.gg/ZYjAwXk"
+            class="d-inline-block social-icon">
+            <font-awesome-icon
+              size="lg"
+              :icon="['fab', 'discord']" />
+          </b-nav-item>
+          <b-nav-item
+            target="_blank"
+            href="https://twitter.com/OpenBitsWorld"
+            class="d-inline-block social-icon">
+            <font-awesome-icon
+              size="lg"
+              :icon="['fab', 'twitter']" />
+          </b-nav-item>
+          <b-nav-item
+            target="_blank"
+            href="https://t.me/openbits_official"
+            class="d-inline-block social-icon">
+            <font-awesome-icon
+              size="lg"
+              :icon="['fab', 'telegram']" />
+          </b-nav-item>
+          <b-nav-item
+            target="_blank"
+            href="https://gitlab.com/cervoneluca/openbits"
+            class="d-inline-block social-icon">
+            <font-awesome-icon
+              size="lg"
+              :icon="['fab', 'gitlab']" />
+          </b-nav-item>
+         </div>
+        <b-nav-item
+          id="main-login-button"
+          right><LayoutLoginModal /></b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+    <b-modal
+      id="test-modal"
+      ref="test-modal"
+      class="w-100 h-100"
+      hide-footer>
       <b-navbar-nav class="ml-auto">
         <b-nav-item-dropdown
           right
@@ -42,7 +148,7 @@
           right
           ><LayoutLoginModal /></b-nav-item>
       </b-navbar-nav>
-    </b-collapse>
+    </b-modal>
   </b-navbar>
 </template>
 
@@ -83,26 +189,39 @@ export default {
 </script>
 
 <style lang="scss">
-  #main-navbar {
-    &.landpage-navbar {
-      z-index: 10000;
-      background: transparent!important;
-    }
-    &.app-navbar {
-      border-bottom: 2px solid black;
-    }
-    .navbar-logo {
-      width:auto;
-      height:70px;
-      display:inline-block!important;
-    }
-    .brand {
-      display:inline-block;
+@import '../../assets/styles/custom-theme.scss';
+
+#main-navbar {
+  &.landpage-navbar {
+    z-index: 10000;
+    background: transparent!important;
+    .nav-item {
+      &:not(#main-login-button) {
+        a {
+          color: $white;
+          font-weight:bold;
+          padding-bottom: 5px;
+          &:hover{
+            color: $secondary-color;
+          }
+        }
+      }
     }
   }
-  /* #main-navbar {
-    z-index:3000!important;
-    position: absolute;
-    width: 100%;
-  } */
+  .social-nav {
+    border-right:1px solid $white;
+    border-left:1px solid $white;
+  }
+  &.app-navbar {
+    border-bottom: 2px solid black;
+  }
+  .navbar-logo {
+    width:auto;
+    height:70px;
+    display:inline-block!important;
+  }
+  .brand {
+    display:inline-block;
+  }
+}
 </style>
